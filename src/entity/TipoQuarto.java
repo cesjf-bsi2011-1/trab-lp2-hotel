9033ad2c1599c5cfc2872ad327ac89f1899bbf8f
Tássio de Oliveira Silva Auad <auadtassio@gmail.com>
2012-10-04 10:26:52 -0300

    TipoQuarto

---
+++ src/entity/TipoQuarto.java
@@ -0,0 +1,56 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tássio Auad
+ */
+public class TipoQuarto {
+    private String codigo;
+    private String nome;
+    private String descricao;
+    private List<Mobilia> mobilias;
+
+    public TipoQuarto(String codigo, String nome, String descricao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        mobilias = new ArrayList<>();
+        
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Mobilia> getMobilias() {
+        return mobilias;
+    }
+
+    public void setMobilias(Mobilia mobilia) {
+        this.mobilias.add(mobilia);
+    }
+    
+}
---
+++ src/mvc/model/dao/TipoQuartoDAO.java
@@ -0,0 +1,66 @@
+package mvc.model.dao;
+
+import entity.Log;
+import entity.TipoQuarto;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tássio Auad
+ */
+public class TipoQuartoDAO extends AbstractDAO{
+    
+    public static List<TipoQuarto> tiposQuarto = new ArrayList<>();
+
+    @Override
+    public void inserir(Object o) {
+        if(o instanceof TipoQuarto) {
+            TipoQuarto tipoQuarto = (TipoQuarto) o;
+            tiposQuarto.add(tipoQuarto);
+            
+        }
+    }
+
+    @Override
+    public void atualizar(String codigo, Object o) {
+        if(o instanceof TipoQuarto) {
+            TipoQuarto tipoQuarto = (TipoQuarto) o;
+            for(int i = 0; i < tiposQuarto.size(); i++) {
+                if(tiposQuarto.get(i).getCodigo().equals(codigo) ) {
+                    tiposQuarto.remove(i);
+                    tiposQuarto.add(tipoQuarto);
+                    
+                }
+            }
+        }
+    }
+
+    @Override
+    public void remover(Object o) {
+        if(o instanceof TipoQuarto) {
+            TipoQuarto tipoQuarto = (TipoQuarto) o;
+            tiposQuarto.remove(tipoQuarto);
+            
+        }
+    }
+
+    @Override
+    public List buscarTodos() {
+        return tiposQuarto;
+        
+    }
+
+    @Override
+    public TipoQuarto buscar(String codigo) {
+        for(int i = 0; i < tiposQuarto.size(); i++) {
+                if(tiposQuarto.get(i).getCodigo().equals(codigo) ) {
+                    return tiposQuarto.get(i);
+                    
+                }
+        }
+        
+        return null;
+    }
+    
+}
