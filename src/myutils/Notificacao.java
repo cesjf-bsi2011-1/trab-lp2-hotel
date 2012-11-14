3057d356e94c4dd4a5ae706dd1e5b1b65e65eba2
Tássio de O. S. Auad <auadtassio@gmail.com>
2012-11-14 17:48:49 -0200

    Classe Notificacao

---
+++ src/myutils/Notificacao.java
@@ -0,0 +1,66 @@
+package myutils;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Tássio Auad
+ */
+public class Notificacao{
+
+    private static Notificacao instance;
+    private Mensagem alerta;
+
+    
+    public static synchronized Notificacao getInstance() 
+    {
+        if (instance == null) {
+            instance = new Notificacao();
+        }
+                
+        return instance;
+    }
+
+    public void exibir(String mensagem) 
+    {
+        alerta = new Mensagem(mensagem);
+        alerta.setVisible(true);
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                alerta.dispose();
+            }
+        }
+        , 3000);
+        
+        
+    }
+
+    private class Mensagem extends JDialog
+    {
+        public Mensagem(String text) {
+            super();
+            JPanel panel = new JPanel();
+            getContentPane().add(panel);
+
+            JLabel mensagem = new JLabel(text);
+            Font font = new Font("Verdana", Font.BOLD, 14);
+            mensagem.setFont(font);
+            mensagem.setForeground(Color.red);
+            
+            panel.add(mensagem);
+            panel.setBackground(Color.yellow);
+            pack();
+            
+        }
+    }
+    
+}
