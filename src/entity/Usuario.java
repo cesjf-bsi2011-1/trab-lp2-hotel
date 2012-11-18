2adf2bcf2c319ad08449aee8ee73b645a7e20075
Tássio de O. S. Auad <auadtassio@gmail.com>
2012-11-18 21:55:46 -0200

    Criado Usuario e UsuarioDAO

--- src/entity/Usuario.java
+++ src/entity/Usuario.java
@@ -1,10 +1,57 @@
 package entity;
 
-/**
- *
- * @author Tássio Auad
- */
 public class Usuario 
 {
+    private int codigo;
+    private String login;
+    private String senha;
+    private String nomeCompleto;
     
+    public Usuario() 
+    {
+
+    }
+
+    public Usuario(String login, String senha, String nomeCompleto) {
+        this.login = login;
+        this.senha = senha;
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
---
+++ src/mvc/model/dao/UsuarioDAO.java
@@ -0,0 +1,174 @@
+package mvc.model.dao;
+
+import entity.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UsuarioDAO extends AbstractDAO{
+
+    private int index = 0;
+    private List<Usuario> listUsuarios = new ArrayList<>();
+    
+    public UsuarioDAO() {
+        
+    }
+    
+    public boolean validarLogin(String lg)
+    {
+        boolean result = false;
+        if(lg.trim().length() >= 4 && lg.trim().length() <= 10) {
+            result = true;
+        } else {
+            result = false;
+        }
+        
+        return result;
+    }
+    
+    public boolean validarSenha(String s)
+    {
+        boolean result = false;
+        if(s.trim().length() >= 5 && s.trim().length() <=10) {
+            result = true;
+        } else {
+            result = false;
+        }
+        
+        return result;
+    }
+    
+    public boolean checkUsuario(Usuario usuario)
+    {
+        String login = usuario.getLogin();
+        String senha = usuario.getSenha();
+        
+        boolean result = false;
+        if(
+                validarLogin(login) 
+                && validarSenha(senha) 
+                && (null != buscarPorLoginESenha(login, senha))
+        ) {
+            result = true;
+        } else {
+            result = false;
+        }
+        
+        return result;        
+    }
+
+    @Override
+    public void inserir(Object o) 
+    {
+        if (objetoEUmUsuario(o)) {
+            Usuario usuarioParaInserir = (Usuario) o;
+            /*Garantindo que o código equivale ao index*/
+            usuarioParaInserir.setCodigo(getIndex());
+            listUsuarios.add(usuarioParaInserir);
+            getHistorico().inserir("Inserção do Usuário " + usuarioParaInserir.getNomeCompleto());
+            acrescerIndex();
+        }       
+    }
+
+    @Override
+    public void remover(Object o) 
+    {
+        if (objetoEUmUsuario(o)) {
+            Usuario usuarioParaRemover = (Usuario) o;
+            listUsuarios.remove((Usuario) o);
+            getHistorico().inserir("Remoção do Usuário " + usuarioParaRemover.getNomeCompleto());
+        }
+    }
+    
+    @Override
+    public void remover(String codigo) 
+    {        
+        Usuario usuarioEncontrado = buscar(codigo);
+        if (null != usuarioEncontrado) {
+            listUsuarios.remove(usuarioEncontrado);
+            getHistorico().inserir("Remoção do Usuário " + usuarioEncontrado.getNomeCompleto());
+        }
+    }
+    
+    @Override
+    public void atualizar(String codigo, Object o) 
+    {
+        if (objetoEUmUsuario(o)) {
+            Usuario clienteParaInserir = (Usuario) o;
+            Usuario clienteParaRemover = buscar(codigo);
+            
+            if (null != clienteParaRemover) {
+                listUsuarios.remove(clienteParaRemover);
+                listUsuarios.add(clienteParaInserir);
+                getHistorico().inserir("Atualização do Usuário " + clienteParaInserir.getNomeCompleto());
+            }
+        }
+    }
+
+    @Override
+    public List buscarTodos() 
+    {
+        return listUsuarios;
+        
+    }
+
+    @Override
+    public Usuario buscar(String codigo) 
+    {
+        for (Usuario clienteDaLista : listUsuarios) {
+                if (clienteDaLista.getCodigo() == Integer.parseInt(codigo)) {
+                    return clienteDaLista;
+                    
+                }
+        }
+        return null;
+        
+    }
+    
+    public Usuario buscarPorLoginESenha(String login, String senha) 
+    {
+        for (Usuario clienteDaLista : listUsuarios) {
+                if (
+                        clienteDaLista.getLogin().equals(login)
+                        && clienteDaLista.getSenha().equals(senha)
+                ) {
+                    return clienteDaLista;   
+                }
+        }
+        return null;
+        
+    }
+  
+    public boolean objetoEUmUsuario(Object o) 
+    {
+        if (o instanceof Usuario) {
+            return true;
+            
+        } else {
+            try {
+                throw new Exception("objetoEUmUsuario(Object o)"
+                        + " recebendo um objeto que não é uma instância"
+                        + " de Usuario");
+            } catch (Exception ex) {
+                Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                return false;
+            }
+        }
+    }
+    
+    @Override
+    public int getIndex()
+    {
+        return index;
+    }
+    
+    @Override
+    public void acrescerIndex()
+    {
+        index += 1;
+    }
+    
+   
+}
